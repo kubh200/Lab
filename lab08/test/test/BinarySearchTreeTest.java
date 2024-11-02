f9456a3c0fd94a2ae2e4c1fffc86a8f26c526ad5
kubh200 <chen.zijie2@northeastern.edu>
2024-11-02 02:41:55 -0400

    Update BinarySearchTreeTest.java
    test add

--- lab08/test/test/BinarySearchTreeTest.java
+++ lab08/test/test/BinarySearchTreeTest.java
@@ -1,19 +1,37 @@
 package test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import bst.BinarySearchTree;
+import bst.BinarySearchTreeImpl;
+
 public class BinarySearchTreeTest {
 
-	@Before
-	public void setUp() throws Exception {
-	}
+	private BinarySearchTree<Integer> bst;
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+    @Before
+    public void setUp() {
+        bst = new BinarySearchTreeImpl<>(); // Using the interface type
+    }
+
+    @Test
+    public void testAddSingleElement() {
+        bst.add(10);
+        assertEquals("Size should be 1 after adding one element", 1, bst.size());
+    }
+
+    @Test
+    public void testAddMultipleElements() {
+        bst.add(10);
+        bst.add(5);
+        bst.add(15);
+        bst.add(3);
+        bst.add(7);
+
+        assertEquals("Size should be 5 after adding five elements", 5, bst.size());
+    }
 
 }
