027b571a3951728742593a663cc60d3875615325
kubh200 <chen.zijie2@northeastern.edu>
2024-11-02 02:02:44 -0400

    Add files via upload

---
+++ lab08/src/bst/BinarySearchTreeImpl.java
@@ -0,0 +1,59 @@
+package bst;
+
+public class BinarySearchTreeImpl<T extends Comparable<T>> implements BinarySearchTree<T> {
+
+	@Override
+	public void add(T data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int height() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean present(T data) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public T minimum() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T maximum() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String preOrder() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String inOrder() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String postOrder() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
