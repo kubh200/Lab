4013f8b8d8c68ecd0a488d10d332a05bad8b3f5b
kubh200 <chen.zijie2@northeastern.edu>
2024-11-02 02:45:14 -0400

    Update BinarySearchTreeImpl.java
    add fuction and addRecursive fuction

--- lab08/src/bst/BinarySearchTreeImpl.java
+++ lab08/src/bst/BinarySearchTreeImpl.java
@@ -2,58 +2,80 @@ package bst;
 
 public class BinarySearchTreeImpl<T extends Comparable<T>> implements BinarySearchTree<T> {
 
-	@Override
-	public void add(T data) {
-		// TODO Auto-generated method stub
-		
-	}
+  private class Node {
+    T data;
+    Node left, right;
 
-	@Override
-	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    Node(T data) {
+      this.data = data;
+      left = right = null;
+    }
+  }
 
-	@Override
-	public int height() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+  private Node root; 
+  private int size;  
 
-	@Override
-	public boolean present(T data) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+  public BinarySearchTreeImpl() {
+    root = null;
+    size = 0;
+  }
 
-	@Override
-	public T minimum() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  @Override
+  public void add(T data) {
+    root = addRecursive(root, data);
+  }
 
-	@Override
-	public T maximum() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  private Node addRecursive(Node node, T data) {
+    if (node == null) {
+      size++;
+      return new Node(data);
+    }
+    if (data.compareTo(node.data) < 0) {
+      node.left = addRecursive(node.left, data);
+    } else if (data.compareTo(node.data) > 0) {
+      node.right = addRecursive(node.right, data);
+    }
+      return node;
+  }
+  
+  @Override
+  public int size() {
+    return size;
+  }
 
-	@Override
-	public String preOrder() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  @Override
+  public int height() {
+    return 0;
+  }
 
-	@Override
-	public String inOrder() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  @Override
+  public boolean present(T data) {
+    return false;
+  }
 
-	@Override
-	public String postOrder() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  @Override
+  public T minimum() {
+    return null;
+  }
+
+  @Override
+  public T maximum() {
+    return null;
+  }
+
+  @Override
+  public String preOrder() {
+    return null;
+  }
+
+  @Override
+  public String inOrder() {
+    return null;
+  }
+
+  @Override
+  public String postOrder() {
+    return null;
+  }
 
 }
